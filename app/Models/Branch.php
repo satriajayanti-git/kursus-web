1f9899640f0f1967beafcd0835aaf1a22f14fb99
muhamadrakan <muhamadrakan22@gmail.com>
2026-08-02 14:58:28 +0700

    add foto to model_branch

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Branch extends Model
 {
     protected $fillable = [
-        'nama_cabang', 
+        'nama_cabang',
+        'foto', 
         'foto_cabang',
         'link_gmaps',    
         'no_telp_admin'
